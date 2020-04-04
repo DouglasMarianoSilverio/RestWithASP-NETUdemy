96e5fdab13e570484dab251b0abb637b32a95da7
Douglas Mariano Silverio <douglas07@gmail.com>
2020-04-04 14:05:08 -0300

    Implementing REST Calculator

---
+++ RestWithASPNETUdemy 01 - Calculator/RestWithASPNETUdemy/Controllers/CalculatorController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestWithASPNETUdemy.Extensions;
+
+namespace RestWithASPNETUdemy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalculatorController : ControllerBase
+    {
+        [HttpGet("{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {
+            if (firstNumber.IsNumeric()&& secondNumber.IsNumeric())
+            {
+                decimal sum = ConvertToDecimal(firstNumber) + ConvertToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid input");
+        }
+
+        private decimal ConvertToDecimal(string value)
+        {
+            decimal decimalValue;
+
+            if (decimal.TryParse(value, out decimalValue)) ;
+
+            return decimalValue;
+        }
+    }
+}
---
+++ RestWithASPNETUdemy 01 - Calculator/RestWithASPNETUdemy/Extensions/CustomExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Extensions
+{
+    public static class CustomExtensions
+    {
+        public static Boolean IsNumeric(this String input)
+        {
+            bool isNumber = double.TryParse(input, System.Globalization.NumberStyles.Any, System.Globalization.NumberFormatInfo.InvariantInfo, out _);
+            return isNumber;
+        }
+    }
+}
