ae97db91dd390be506ca324edaa519e75491f86b
Douglas Mariano Silverio <douglas07@gmail.com>
2020-04-04 14:46:53 -0300

    implementing all verbs - begin

---
+++ RestWithASPNETUdemy 01 - Persons/RestWithASPNETUdemy/Models/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestWithASPNETUdemy.Models
+{
+	public enum Gender
+	{
+		Masculino =1,
+		Feminino = 2
+	}
+}
